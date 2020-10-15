df54d859bc23ac2b20787701f50a80688ed08120
Tim Hradil <timhradil@gmail.com>
2020-10-15 22:57:59 +0000

    Imported and described iris.arff

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
+import java.io.File;
+import jsat.ARFFLoader;
+import jsat.DataSet;
+import jsat.classifiers.DataPoint;
+
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+        File file = new File("iris.arff");
+		DataSet dataSet = ARFFLoader.loadArffFile(file);
+
+		System.out.println("The categorical features are:");
+		for(int i = 0; i < dataSet.getNumCategoricalVars(); i++)
+			System.out.println("\t" + dataSet.getCategoryName(i));
+		
+		System.out.println("\nThe numerical features are:");
+		for(int i = 0; i < dataSet.getNumNumericalVars(); i++)
+			System.out.println("\t" + dataSet.getNumericName(i));
+		
+		System.out.println("\nThere are "+dataSet.getSampleSize()+" samples.\n");
+
+		System.out.print("The first 10 samples are:\n");
+		for(int i = 0; i < 10; i++)
+			System.out.println(dataSet.getDataPoint(i));
+    }
+
+	
 }
